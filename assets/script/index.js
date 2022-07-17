686df52218767df1abc26f14b7132de7e6ee7942
raysantori <rayssacgomess@gmail.com>
2022-07-17 17:26:51 -0300

    fix. text copyright

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,4 +2,4 @@
 var copyright = document.querySelector('#copyright')
 var dataAtual = new Date()
 var anoAtual = dataAtual.getFullYear()
-copyright.innerHTML = `Todos dos direitos reservados &copy ${anoAtual} ProntoAR Refrigeração Comercial e Residencial`
+copyright.innerHTML = `Todos os direitos reservados &copy ${anoAtual} ProntoAR Refrigeração Comercial e Residencial`
