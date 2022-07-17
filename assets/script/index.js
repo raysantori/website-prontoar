eca39b53c5c26b7ee1eb193567cb2ae3fcdc40d8
raysantori <rayssacgomess@gmail.com>
2022-07-17 14:38:05 -0300

    feat. contato html

---
+++ assets/script/index.js
@@ -0,0 +1,5 @@
+//footer
+var copyright = document.querySelector('#copyright')
+var dataAtual = new Date()
+var anoAtual = dataAtual.getFullYear()
+copyright.innerHTML = `&copy ${anoAtual} ProntoAR Refrigeração Comercial e Residencial`
